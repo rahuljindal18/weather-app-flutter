4889ad7e79003466f7468e1df4707ddbef49affb
rahuljindal18 <rahuljindal1989@gmail.com>
2020-05-04 11:51:49 +0530

    weather app flutter

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import '../services/weather.dart';
+import './location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+import 'location_screen.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    WeatherModel model = WeatherModel();
+    var weatherData = await model.getLocationWeather();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => LocationScreen(
+          locationWeather: weatherData,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
